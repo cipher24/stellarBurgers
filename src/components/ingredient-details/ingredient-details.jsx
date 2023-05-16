2d917ce1f4d9bd6701fdc571a20e2748cb1358ba
cipher24 <stomachjungle@gmail.com>
2023-05-16 18:54:18 +0300

    added divs to correct render fats & etc.

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -7,22 +7,22 @@ const IngredientDetails = ({ ingredient }) => {
       <img src={ingredient.image_large} alt='ingredient photo'></img>
       <p className={`mt-4 mb-8 text text_type_main-medium`}>{ingredient.name}</p>
       <div className={`${styles.nutrition} text text_type_main-default text_color_inactive mb-15`}>
-        <>
+        <div>
           <p>Калории,ккал</p>
           <p className='text_type_digits-default'>{ingredient.calories}</p>
-        </>
-        <>
+        </div>
+        <div>
           <p>Белки, г</p>
-          <p  className={`${styles.text} text_type_digits-default`}>{ingredient.proteins}</p>
-        </>
-        <>
+          <p className={`${styles.text} text_type_digits-default`}>{ingredient.proteins}</p>
+        </div>
+        <div>
           <p>Жиры, г</p>
-          <p  className='text_type_digits-default'>{ingredient.fat}</p>
-        </>
-        <>
+          <p className='text_type_digits-default'>{ingredient.fat}</p>
+        </div>
+        <div>
           <p>Углеводы, г</p>
           <p className='text_type_digits-default'>{ingredient.carbohydrates}</p>
-        </>
+        </div>
       </div>
     </div>
   )
